964ebfc51d90a229d5d1818de54c779726f119e8
Marek Pedrys <marek.pedrys@gmail.com>
2023-11-12 17:02:44 +0100

    modified Lab7zad1

--- src/lab7/Lab7zad1.java
+++ src/lab7/Lab7zad1.java
@@ -1,32 +1,49 @@
 package lab7;
 
+//        Napisz program, który dla dowolnej liczby całkowitej n z przedziału (1 ≤ n ≤ 30) wypisuje macierz jak w poniższych przykładach. Użyj dwóch zagnieżdżonych pętli.
+//
+//        n = 2	matrix
+//        --------------
+//        1  2
+//        2  3
+//
+//        n = 3	matrix
+//        --------------
+//        1  2  3
+//        2  3  4
+//        3  4  5
+//
+//        n = 4	matrix
+//        --------------
+//        1  2  3  4
+//        2  3  4  5
+//        3  4  5  6
+//        4  5  6  7
+
 public class Lab7zad1 {
     public static void main(String[] args) {
-//        int n = 4;
+        int n = 4;
+        System.out.println("n = " + n + "\tmatrix");
+        System.out.println("--------------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i + j + "\t");
+            }
+            System.out.println();
+        }
+
+//        int n = 8;
 //        System.out.println("n = " + n + " matrix");
-//        System.out.println("---------------");
-//        for (int i = 1; i <= n; i++) {
-//            for (int number = i, counter = 0; counter < n; number++, counter++) {
-//                System.out.printf("%d\t", number);
+//        for (int i = 0; i < 3 * n; i++) {
+//            System.out.print("-");
+//        }
+//        System.out.println();
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 1; j <= n; j++) {
+//                System.out.printf("%3s", i + j + " ");
 //            }
 //            System.out.println();
 //        }
 
-
-        int n = 8;
-        System.out.println("n = " + n + " matrix");
-        for (int i = 0; i < 3*n; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.printf("%3s", i + j + " ");
-            }
-            System.out.println();
-        }
     }
-
-
 }
-
