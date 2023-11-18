3c49794ca333226f74c89a5bf68d214c1d3f6941
Marek Pedrys <marek.pedrys@gmail.com>
2023-11-18 19:26:42 +0100

    zadanieDomomowe2 zad1

---
+++ src/zadanieDomomowe2/Zad1.java
@@ -0,0 +1,46 @@
+package zadanieDomomowe2;
+
+//      Napisz program, który:
+//        stworzy tablicę liczb typu int o rozmiarze, który będziemy podawać jako argument przy starcie programu,
+//        wypełni tablicę kolejno liczbami od 1 do n, gdzie n to rozmiar tablicy,
+//        wyświetli zawartość tablicy,
+//        dokona konwersji tablicy tak, aby elementy ułożyły się odwrotnie (ciąg malejący: n, n-1, n-2… 1),
+//        wyświetli zawartyość przekonwertowanej tablicy,
+//        fragmenty programu odpowiadjące za konkretne zadania zostaną wyodrębnione jako oddzielne metody.
+
+import java.util.Arrays;
+
+public class Zad1 {
+
+    public static void main(String[] args) {
+        int arg = Integer.parseInt(args[0]);
+        int[] ints = createArray(arg);
+        fillInArray(ints);
+        printArray(ints);
+        convertArray(ints);
+        printArray(ints);
+    }
+
+    private static void convertArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int tempI = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = tempI;
+        }
+    }
+
+    private static void printArray(int[] ints) {
+        System.out.println(Arrays.toString(ints));
+    }
+
+    private static void fillInArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+    }
+
+    private static int[] createArray(int size) {
+        return new int[size];
+    }
+
+}
