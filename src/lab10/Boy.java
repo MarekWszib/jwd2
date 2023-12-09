d86638fb514d61334f45834ffd7240af97580706
Marek Pedrys <pedrys@student.wszib.edu.pl>
2023-12-09 10:48:56 +0100

    lab10

---
+++ src/lab10/Boy.java
@@ -0,0 +1,16 @@
+package lab10;
+
+public class Boy extends Human {
+    @Override
+    void eat() {
+        System.out.println("Boy is eating...");
+    }
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.eat();
+
+        Boy boy = new Boy();
+        boy.eat();
+    }
+}
---
+++ src/lab10/Human.java
@@ -0,0 +1,7 @@
+package lab10;
+
+public class Human {
+    void eat() {
+        System.out.println("Human is eating...");
+    }
+}
