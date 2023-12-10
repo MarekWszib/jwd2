fa910cf3a6aa675f193c5547b5a0bca9e0cee38c
Marek Pedrys <pedrys@student.wszib.edu.pl>
2023-12-10 13:00:04 +0100

    visit counter

---
+++ src/modifiers/VisitCounter.java
@@ -0,0 +1,13 @@
+package modifiers;
+
+public class VisitCounter {
+    private int counter;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increment() {
+        counter++;
+    }
+}
---
+++ src/modifiers/other/VisitCounterTester.java
@@ -0,0 +1,10 @@
+package modifiers.other;
+
+import modifiers.VisitCounter;
+
+public class VisitCounterTester {
+    public static void main(String[] args) {
+        VisitCounter vc = new VisitCounter();
+
+    }
+}
