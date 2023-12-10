d3e859a98a718de060b17f4802c2de0997a1a7d1
Marek Pedrys <pedrys@student.wszib.edu.pl>
2023-12-10 14:35:21 +0100

    modified modifiers

--- src/modifiers/Demo.java
+++ src/modifiers/Demo.java
@@ -1,11 +1,16 @@
 package modifiers;
 
 public class Demo {
+
+    public static final int NUMBER = 6;
     static synchronized protected final void specialMethod() {
     }
-    public static final int NUMBER = 5;
 
     public static void main(String[] args) {
-
+        final int number;
+        System.out.println("start");
+        number = 1;
+//        number = 2;
+        System.out.println("koniec");
     }
 }
