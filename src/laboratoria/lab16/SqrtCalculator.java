cec6fe84ceb377483edf855599b7801777fe2748
Marek Pedrys <marek.pedrys@gmail.com>
2024-02-17 20:54:23 +0100

    refactored lab16 SqrtCalculator

--- src/laboratoria/lab16/SqrtCalculator.java
+++ src/laboratoria/lab16/SqrtCalculator.java
@@ -1,6 +1,6 @@
 package laboratoria.lab16;
 
-//Napisz program, który pobierze od użytkownika liczbę i wyświetli jej pierwiastek.
+//        Napisz program, który pobierze od użytkownika liczbę i wyświetli jej pierwiastek.
 //        Do obliczenia pierwiastka możesz użyć istniejącej metody java.lang.Math.sqrt().
 //        Jeśli użytkownik poda liczbę ujemną rzuć wyjątek java.lang.IllegalArgumentException.
 //        Obsłuż sytuację, w której użytkownik poda ciąg znaków, który nie jest liczbą.
@@ -12,44 +12,17 @@ public class SqrtCalculator {
     // dobra wersja
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double d = 0;
-
-
+        double userInput = 0;
+        System.out.println("Podaj liczbę:");
         try {
-            d = Double.parseDouble(scanner.nextLine());
+            userInput = Double.parseDouble(scanner.nextLine());
         } catch (NumberFormatException e) {
-            System.out.println("Błąd! Podana wartość nie jest liczbą!");
+            System.out.println("Podany ciąg znaków nie jest liczbą!");
             System.exit(-1);
         }
-
-        if (d < 0) {
+        if (userInput < 0) {
             throw new IllegalArgumentException();
         }
-        System.out.println(Math.sqrt(d));
-
+        System.out.println("Pierwiastek z " + userInput + " wynosi " + Math.sqrt(userInput));
     }
-
-
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        Double userInput = 0d;
-//        System.out.println("Podaj liczbę:");
-//
-//        try {
-//            userInput = scanner.nextDouble();
-//        } catch (Exception e) {
-//            System.out.println("Podano ciąg znaków, który nie jest liczbą!");
-//            System.exit(-1);
-//        }
-//
-//        if (userInput < 0) {
-//            throw new IllegalArgumentException("Podano liczbę ujemną: " + userInput);
-//        }
-//
-//
-//        System.out.println("Pierwiastek z " + userInput + " wynosi: " + Math.sqrt(userInput));
-//
-//
-//    }
-
 }
