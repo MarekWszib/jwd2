f9f23ca706f0e8dde95c3b776c7217693c5da059
Marek Pedrys <marek.pedrys@gmail.com>
2024-06-29 21:28:27 +0200

    lab21 modified

--- src/laboratoria/lab21/PlanetsSortingByLastChar.java
+++ src/laboratoria/lab21/PlanetsSortingByLastChar.java
@@ -3,8 +3,9 @@ package laboratoria.lab21;
 import java.util.Arrays;
 import java.util.Comparator;
 
-//Posortuj tablicę nazw planet układu słonecznego stosując wyrażenie lambda.
-// Sortowanie alfabetyczne po ostatniej literze w nazwie planety.
+//  Posortuj tablicę nazw planet układu słonecznego stosując wyrażenie lambda.
+//  Sortowanie alfabetyczne po ostatniej literze w nazwie planety.
+
 public class PlanetsSortingByLastChar {
     public static void main(String[] args) {
         String[] planets = {"Merkury", "Wenus", "Ziemia", "Mars", "Jowisz", "Saturn", "Uran", "Neptun"};
